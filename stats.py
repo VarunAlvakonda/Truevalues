4090b0adca1bd5010ad36d22cb6253c4cfd3f40c
VarunAlvakonda <33843670+VarunAlvakonda@users.noreply.github.com>
2025-07-27 20:37:20 +0100

    t

--- stats.py
+++ stats.py
@@ -96,7 +96,7 @@ def matchfactor(data,criteria,Position,typeoffactor):
         ).reset_index()
 
 
-        batting = pd.merge(df_match_totals, df_match_totals2, on=['Team','Inns','Start Date','Host Country','year'], suffixes=('', '_grouped'))
+        batting = pd.merge(df_match_totals, df_match_totals2, on=['Team','Start Date','Host Country','year'], suffixes=('', '_grouped'))
 
     batting['cen_diff'] = batting['Centuries_grouped'] - batting['Centuries']
     batting['FiftiesPlus_diff'] = batting['Fifties_grouped']+batting['Centuries_grouped'] - batting['Fifties'] - batting['Centuries']
