0e05a341ee0cfe3018b1c8e6109d25dd3a482252
VarunAlvakonda <33843670+VarunAlvakonda@users.noreply.github.com>
2025-09-12 13:20:33 +0100

    t

--- BattingTrueValues.py
+++ BattingTrueValues.py
@@ -343,10 +343,23 @@ def main():
         options = ['Overall Stats', 'Season By Season']
         choice = st.sidebar.selectbox('Select your option:', options)
         choice2 = st.sidebar.selectbox('Individual Player or Everyone:', ['Individual', 'Everyone'])
+        data['Start Date'] = pd.to_datetime(data['Date'], errors='coerce')
+        valid_dates = data['Start Date'].dropna()
+        # valid_dates = data
+        import datetime
+        if not valid_dates.empty:
+            min_dt = valid_dates.min().date()
+            max_dt = valid_dates.max().date()
+        else:
+            min_dt = datetime.date(2000, 1, 1)
+            max_dt = datetime.date(2030, 1, 1)
 
-        # User inputs for date range
-        start_date = st.sidebar.date_input('Start date', data['Date'].min())
-        end_date = st.sidebar.date_input('End date', data['Date'].max())
+        start_date = st.sidebar.date_input("Start date", min_value=min_dt, max_value=max_dt, value=min_dt)
+        end_date = st.sidebar.date_input("End date", min_value=min_dt, max_value=max_dt, value=max_dt)
+
+        # # User inputs for date range
+        # start_date = st.sidebar.date_input('Start date', data['Date'].min())
+        # end_date = st.sidebar.date_input('End date', data['Date'].max())
 
         # Filtering data based on the user's date selection
         if start_date > end_date:
