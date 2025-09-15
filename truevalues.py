ab4cc1e239daa1a996f408b86006f8874a627bc9
VarunAlvakonda <33843670+VarunAlvakonda@users.noreply.github.com>
2025-09-15 20:41:05 +0100

    t

--- truevalues.py
+++ truevalues.py
@@ -334,8 +334,8 @@ def main():
 
 
     # Load data with caching
-    allt20s = load_data('T20Data.parquet')  # Changed to parquet
-
+    allt20s = load_data('T20Dataoptimized.parquet')  # Changed to parquet
+    print(allt20s.dtypes)
     selected_leagues = st.sidebar.multiselect('Choose leagues:', allt20s['CompName'].unique())
 
     if selected_leagues:
