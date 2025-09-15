a1bfce83c6f0e15bd22a390aa9c42836d778027d
VarunAlvakonda <33843670+VarunAlvakonda@users.noreply.github.com>
2025-09-15 20:48:20 +0100

    t

--- truevalues.py
+++ truevalues.py
@@ -4,7 +4,7 @@ import streamlit as st
 from scipy.optimize import curve_fit
 
 place = 'Venue_x'
-place = 'Country'
+# place = 'Country'
 type = 'PhaseofSeason'
 type = 'BowlCat'
 type = 'entryphase'
