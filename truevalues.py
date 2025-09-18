cb54b0626a6337095102a45c8c3e17a64688f0dc
VarunAlvakonda <33843670+VarunAlvakonda@users.noreply.github.com>
2025-09-18 16:49:43 +0100

    t

--- truevalues.py
+++ truevalues.py
@@ -452,15 +452,10 @@ def load_data(filename):
 def main():
     st.sidebar.title('True Values')
 
-    format_choice = st.sidebar.selectbox('Select Format:', ['T20s', 'ODI'])
-    if format_choice == 'T20s':
-        # Load data with caching
-        allt20s = load_data('T20DataT20Leagues_optimized.parquet')  # Changed to parquet
-    else:
-        allt20s = load_data('ODIData_optimized.parquet')  # Changed to parquet
 
+    # Load data with caching
+    allt20s = load_data('T20DataT20Leagues_optimized.parquet')  # Changed to parquet
     selected_leagues = st.sidebar.multiselect('Choose leagues:', allt20s['CompName'].unique())
-
 
     if selected_leagues:
         data = allt20s[allt20s['CompName'].isin(selected_leagues)]
