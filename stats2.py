855c692c569930b6a65f0026c527c3a7c7063919
VarunAlvakonda <33843670+VarunAlvakonda@users.noreply.github.com>
2026-02-04 21:59:50 +0530

    Update stats2.py

--- stats2.py
+++ stats2.py
@@ -42,7 +42,7 @@ def batadjstats(df, start_date, end_date):
 
     filtered_data3["IsKeeper"] = filtered_data3["IsKeeper"].fillna("No")
 
-    choice4 = st.sidebar.multiselect("Result:", filtered_data3["Result"].unique())
+    choice4 = st.multiselect("Result:", filtered_data3["Result"].unique())
     if choice4:
         filtered_data3 = filtered_data3[filtered_data3["Result"].isin(choice4)]
 
